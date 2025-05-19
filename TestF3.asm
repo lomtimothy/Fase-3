30f99d418987fc6c38b88f896252ce67997e2a38
lomtimothy <alvaradogael576@gmail.com>
2025-05-18 22:52:15 -0600

    Add files via upload

---
+++ TestF3.asm
@@ -0,0 +1,21 @@
+nop
+addi $t0, $zero, 1337
+nop
+J 0
+nop
+nop
+addi $t0, $t0, -724
+nop
+J 12
+nop
+nop
+nop
+addi $t0, $t0, -7
+nop
+J 6
+nop
+nop
+nop
+addi $t0, $t0, -605
+nop
+sw $t0, 0($zero)
